c572fcb03aa3f524b23d9890b2601d45fe55e9cb
ckwang19 <ckwang19@gmail.com>
2019-09-17 23:39:44 +0800

    add topk_volume and sorted with top

--- bombom.py
+++ bombom.py
@@ -40,10 +40,11 @@ import copy
 #assert False
 
 # self.ma_days = 200
-# stock_dict = {	supported_point: {  Interval: xxx 
-#										vol_val: xxx	},
-#					topk_vol:		[{'volume': max_volume, 'close': max_volume_close}, 
-#									{'volume': max_volume2, 'close': max_volume_close2}....],
+# stock_dict = {	supported_point: [{  Interval: xxx 
+#										vol_val: xxx	}, {...}, {...}],
+
+#					topk_vol:		[0, 0, top1_volume, 0, 0, 0, top2_volume.....],
+
 #					moving_average: {'2012-02-14': {'type': big_cow,
 #													'close': xxx,
 #													'MA5': xxx,
@@ -69,11 +70,13 @@ class Trader(object):
 		self.value_group = -1
 		self.top_volume_num = 10
 		self.part_num = 100
+		self.ma_days = 200
 
 #
 
 	def get_supporting_point(self, stock_name, file_path):
-		stock_dict_sum = {'supported_point':{},'topk_vol':[],'moving_average':{}}
+		stock_dict_sum = {'topk_vol':[],'supported_point':{},'moving_average':{}, 'MA_state_dict':{}}
+		#stock_dict_sum = {'moving_average':{}}
 		stock_dict = {}
 		stock_close_list = []
 		press_list = []
@@ -99,101 +102,197 @@ class Trader(object):
 				stock_close_list.append(float(Close))
 				stock_volume_list.append(int(Volume))
 				stock_date_list.append(Date)
+		self.interval_value = round(max(stock_close_list) / self.part_num, 3)
 		#print (stock_dict)
 		#print (stock_name, 'get_supporting_point')
+		topk_volume_list = [0]*self.part_num
+		for key in stock_dict_sum.keys():
+			if key == 'topk_vol':
+				# topk volume
+				#topk_volume_list = [0]*self.part_num
+				stock_volume_list_tmp = copy.deepcopy(stock_volume_list)
 
-		# top3 volume
-		top3_volume_list = []
-		stock_volume_list_tmp = copy.deepcopy(stock_volume_list)
-		for num in range(self.top_volume_num):
-			max_volume = max(stock_volume_list_tmp)
-			stock_volume_list_tmp.remove(max_volume)
-			max_idx = stock_volume_list.index(max_volume)
-			max_date = stock_date_list[max_idx]
-			max_volume_close = stock_close_list[max_idx]
-			top3_volume_list.append({max_date: {'volume': max_volume, \
-												'close': max_volume_close}})
-		print (top3_volume_list)
+				for num in range(self.top_volume_num):
+					max_volume = max(stock_volume_list_tmp)
+					stock_volume_list_tmp.remove(max_volume)
+					max_idx = stock_volume_list.index(max_volume)
+					max_date = stock_date_list[max_idx]
+					max_volume_close = stock_close_list[max_idx]
+					#topk_volume_list.append({max_date: {'volume': max_volume, \
+					#									'close': max_volume_close}})
+					#print (int(max_volume_close//(max_volume_close // self.part_num)))
+					topk_volume_list[int(max_volume_close/(self.interval_value))] += max_volume
+				print (topk_volume_list)
 
-		"""
-		# press
-		for idx, Close in enumerate(stock_close_list):
-			if idx < self.period_days or idx+self.period_days > len(stock_close_list):
-				continue
-			Close_five_days_pass_min = min(stock_close_list[idx-self.period_days:idx])
-			if not Close > Close_five_days_pass_min*1.1:
-				continue
-			Close_five_days_pass_max = max(stock_close_list[idx-self.period_days:idx])
-			if not Close > Close_five_days_pass_max:
-				continue
-			Close_five_days_next_max = max(stock_close_list[idx+1:idx+1+self.period_days])
-			if not Close > Close_five_days_next_max:
-				continue
-			Close_five_days_next_min = min(stock_close_list[idx+1:idx+1+self.period_days])
-			if not Close > Close_five_days_next_min*1.1:
-				continue
-			#print (idx, Close)
-			press_dict = {'Date': stock_date_list[idx],
-							'Volume_Value': Close*stock_dict[stock_date_list[idx]]['Volume'],
-							'Close': Close}
-			press_list.append(press_dict)
-		print (press_list)
-		"""
+				stock_dict_sum['topk_vol'] = topk_volume_list
+			if key == 'press':
+				# press
+				for idx, Close in enumerate(stock_close_list):
+					if idx < self.period_days or idx+self.period_days > len(stock_close_list):
+						continue
+					Close_five_days_pass_min = min(stock_close_list[idx-self.period_days:idx])
+					if not Close > Close_five_days_pass_min*1.1:
+						continue
+					Close_five_days_pass_max = max(stock_close_list[idx-self.period_days:idx])
+					if not Close > Close_five_days_pass_max:
+						continue
+					Close_five_days_next_max = max(stock_close_list[idx+1:idx+1+self.period_days])
+					if not Close > Close_five_days_next_max:
+						continue
+					Close_five_days_next_min = min(stock_close_list[idx+1:idx+1+self.period_days])
+					if not Close > Close_five_days_next_min*1.1:
+						continue
+					#print (idx, Close)
+					press_dict = {'Date': stock_date_list[idx],
+									'Volume_Value': Close*stock_dict[stock_date_list[idx]]['Volume'],
+									'Close': Close}
+					press_list.append(press_dict)
+				#print (press_list)
+			if key == 'supported_point':
+				support_list = []
+				# support
+				Volume_Value_max = -1
+				Volume_max = -1
+				for idx, Close in enumerate(stock_close_list):
+					if idx < self.period_days or idx+self.period_days > len(stock_close_list):
+						continue
 
-		support_list = []
-		# support
-		for idx, Close in enumerate(stock_close_list):
-			if idx < self.period_days or idx+self.period_days > len(stock_close_list):
-				continue
+					Close_five_days_pass_min = min(stock_close_list[idx-self.period_days:idx])
+					if not Close < Close_five_days_pass_min:
+						continue
+					Close_five_days_pass_max = max(stock_close_list[idx-self.period_days:idx])
+					if not Close < Close_five_days_pass_max:
+						continue
+					Close_five_days_next_max = max(stock_close_list[idx+1:idx+1+self.period_days])
+					if not Close < Close_five_days_next_max*1.1:
+						continue
+					Close_five_days_next_min = min(stock_close_list[idx+1:idx+1+self.period_days])
+					if not Close < Close_five_days_next_min:
+						continue
+					#print (idx, Close)
+					support_dict = {'Date': stock_date_list[idx],
+									'Volume_Value': Close*stock_dict[stock_date_list[idx]]['Volume'],
+									'Volume': stock_dict[stock_date_list[idx]]['Volume'],
+									'Close': Close}
+					Volume_Value_max = Close*stock_dict[stock_date_list[idx]]['Volume'] if Close*stock_dict[stock_date_list[idx]]['Volume'] > Volume_Value_max else Volume_Value_max
+					Volume_max = stock_dict[stock_date_list[idx]]['Volume'] if stock_dict[stock_date_list[idx]]['Volume'] > Volume_max else Volume_max
+					support_list.append(support_dict)
+				#print (support_list)
 
-			Close_five_days_pass_min = min(stock_close_list[idx-self.period_days:idx])
-			if not Close < Close_five_days_pass_min:
-				continue
-			Close_five_days_pass_max = max(stock_close_list[idx-self.period_days:idx])
-			if not Close < Close_five_days_pass_max:
-				continue
-			Close_five_days_next_max = max(stock_close_list[idx+1:idx+1+self.period_days])
-			if not Close < Close_five_days_next_max*1.1:
-				continue
-			Close_five_days_next_min = min(stock_close_list[idx+1:idx+1+self.period_days])
-			if not Close < Close_five_days_next_min:
-				continue
-			#print (idx, Close)
-			support_dict = {'Date': stock_date_list[idx],
-							'Volume_Value': Close*stock_dict[stock_date_list[idx]]['Volume'],
-							'Close': Close}
-			support_list.append(support_dict)
-		#print (support_list)
+				#print (support_list)
+				#assert False
+				support_all_dict = {}
+				topk_volume_all_dict = {}
+				for num in range(self.part_num):
+					support_all_dict['{}_{}'.format(round(self.interval_value*num, 3), round(self.interval_value*(num+1), 2))] = 0
+					topk_volume_all_dict['{}_{}'.format(round(self.interval_value*num, 3), round(self.interval_value*(num+1), 2))] = 0
+				for support_dict_tmp in support_list:
+					num = int(support_dict_tmp['Close'] / self.interval_value)
+					support_all_dict['{}_{}'.format(round(self.interval_value*num, 3), round(self.interval_value*(num+1), 2))] \
+						+=(support_dict_tmp['Volume_Value']) / float(Volume_Value_max)
+				for idx, value in enumerate(topk_volume_list):
+					topk_volume_all_dict['{}_{}'.format(round(self.interval_value*idx, 3), round(self.interval_value*(idx+1), 2))] \
+						+=value / float(Volume_max)
+				#print (support_all_dict)
+				#print (topk_volume_all_dict)
+				#assert False
+						#sorted by supported volume * close value
+						#+=support_dict_tmp['Volume_Value']
 
-		support_all_dict = {}
-		part_value = round(max(stock_close_list) / self.part_num, 3)
-		for num in range(self.part_num):
-			support_all_dict['{}_{}'.format(round(part_value*num, 3), round(part_value*(num+1), 2))] = 0
-		for support_dict_tmp in support_list:
-			num = int(support_dict_tmp['Close'] / part_value)
-			support_all_dict['{}_{}'.format(round(part_value*num, 3), round(part_value*(num+1), 2))] \
-				+=support_dict['Volume_Value']
-
-		#print (support_all_dict)
-		support_all_list = []
-		for key in support_all_dict.keys():
-			if support_all_dict[key] == 0:
-				continue
-			if support_all_list == []:
-				support_all_list.append({'Interval': key, \
-										'Volume_Value': support_all_dict[key]})
-			else:
-				insert_idx = 0
-				for support_dict in support_all_list:
-					#print (support_dict)
-					#print (support_dict['Volume_Value'], support_all_dict[key])
-					if support_dict['Volume_Value'] >= support_all_dict[key]:
-						insert_idx += 1
+				#print (support_all_dict)
+				support_all_list = []
+				for key in topk_volume_all_dict.keys():
+					#if support_all_dict[key] == 0:
+					#	continue
+					if support_all_list == []:
+						support_all_list.append({'Interval': key, \
+												'Volume_Value': support_all_dict[key], \
+												'topk_volume': topk_volume_all_dict[key]})
 					else:
-						break
-				support_all_list.insert(insert_idx, {'Interval': key, \
-										'Volume_Value': support_all_dict[key]})
-		print (support_all_list)
+						insert_idx = 0
+						for support_dict in support_all_list:
+							# sorted by supported volume * close value
+							#if support_dict['Volume_Value'] >= support_all_dict[key]:
+							# sorted by topk volume
+							# 現在要加入的support_all_dict[key]要加到idx多少
+							if support_dict['topk_volume'] >= topk_volume_all_dict[key]:
+								insert_idx += 1
+							else:
+								break
+						support_all_list.insert(insert_idx, {'Interval': key, \
+												'Volume_Value': support_all_dict[key], \
+												'topk_volume': topk_volume_all_dict[key]})
+				print (support_all_list)
+				stock_dict_sum['supported_point'] = support_all_list
+			if key == 'moving_average':
+#					moving_average: {'2012-02-14': {'type': big_cow,
+#													'close': xxx,
+#													'MA5': xxx,
+#													'MA20': xxx,
+#													'MA40': xxx,
+#													'MA80': xxx },
+#									'2012-02-1X': {'type': small_bear,
+#													'close': xxx,
+#													'MA5': xxx,
+#													'MA20': xxx,
+#													'MA40': xxx,
+#													'MA80': xxx },
+#									....}
+				MA_dict = {}
+				MA_dict_all = {}
+				MA_state_dict = {}
+				first_enter = True
+				MA40_state = 0
+				MA80_state = 0
+				change_state = False
+				for idx in range(len(stock_close_list)-1, len(stock_close_list)-self.ma_days, -1):
+					close = stock_close_list[idx]
+					# 1 2 3 4 5 6 7 8 len=8 [3:8]->[idx-ma+1:idx+1]
+					MA5 = round(sum(stock_close_list[idx-5:idx+1]) / 5.0, 2)
+					MA20 = round(sum(stock_close_list[idx-20:idx+1]) / 20.0, 2)
+					MA40 = round(sum(stock_close_list[idx-40:idx+1]) / 40.0, 2)
+					MA80 = round(sum(stock_close_list[idx-80:idx+1]) / 80.0, 2)
+
+					situation_type = 'big_cow' if MA5 > MA20 > MA40 > MA80 else 'small_cow' if MA40 > MA80 \
+									else 'big_bear' if MA5 < MA20 < MA40 < MA80 else 'small_bear'
+					MA_dict = {'situation_type': situation_type, \
+								'close': close, \
+								'MA5': MA5, \
+								'MA20': MA20, \
+								'MA40': MA40, \
+								'MA80': MA80 }
+					MA_dict_all['{}'.format(stock_date_list[idx])] = MA_dict
+					
+					if first_enter:
+						first_enter = False
+					else:
+						MA40_state = 1 if MA40 > MA40_last else -1
+						MA80_state = 1 if MA80 > MA80_last else -1
+						if MA_state_dict == {}:
+							MA_state_dict = {'MA40_state': 0, 'MA80_state': 0}
+
+						else:
+							if MA40_state == MA40_state_last and not change_state:
+								MA_state_dict['MA40_state'] += 1
+							else:
+								change_state = True
+							if MA80_state == MA80_state_last and not change_state:
+								MA_state_dict['MA80_state'] += 1
+							else:
+								change_state = True
+						MA40_state_last = MA40_state
+						MA80_state_last = MA80_state
+					MA40_last, MA80_last, = MA40, MA80
+
+
+
+				#print (MA_dict_all)
+				stock_dict_sum['moving_average'] = MA_dict_all
+				stock_dict_sum['MA_state_dict'] = MA_state_dict
+
+		#print (stock_dict_sum)
+				
+
 
 # 用基本面篩選
 # MA 40 80負斜率持續 70天就不要
